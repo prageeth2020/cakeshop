639e1bc3fa1ec05dc7b854feb0664e490720341f
prageeth2020 <prageethpramuditha.2020@gmail.com>
2020-06-14 18:54:57 +0530

    issue fixed

---
+++ src/Components/navBar.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+
+class NavBar extends Component {
+    constructor() {
+        super();
+
+
+        this.state = {
+            home : "nav-item active mx-2 ",
+            OurService : "nav-item  mx-2",
+            ContactUs : "nav-item  mx-2"
+
+        };
+
+
+    }
+
+    Home = () => {
+        this.setState({
+            home : "nav-item active mx-2",
+            OurService : "nav-item  mx-2",
+            ContactUs : "nav-item  mx-2"
+        })
+    }
+    OurService = () => {
+        this.setState({
+            home : "nav-item  mx-2",
+            OurService : "nav-item active mx-2",
+            ContactUs : "nav-item  mx-2"
+        })
+    }
+    ContactUs = () => {
+        this.setState({
+            home : "nav-item  mx-2",
+            OurService : "nav-item  mx-2",
+            ContactUs : "nav-item active mx-2"
+        })
+    }
+    render() {
+        return (
+            <div className="">
+                <nav className="navbar navbar-expand-lg navbar-light clr ">
+                    <div className="container">
+                        <b className="navBar">Study Support</b>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false"
+                                aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div className="collapse navbar-collapse sideSpace" id="navbarTogglerDemo01">
+
+                            <ul className="navbar-nav my-2 my-lg-0 App1  ">
+                                <li className= {this.state.home } >
+                                    <p className="nav-link rcorners1" >Home</p>
+
+                                </li>
+                                <li className= {this.state.OurService } >
+                                    <p className="nav-link " >Products</p>
+                                </li>
+                                <li className= {this.state.ContactUs } >
+                                    <p className="nav-link " >Contact Us</p>
+                                </li>
+                                <li className= {this.state.ContactUs } >
+                                    <p className="nav-link " >About Us</p>
+                                </li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </nav>
+
+            </div>
+        );
+    }
+}
+
+export default NavBar;
