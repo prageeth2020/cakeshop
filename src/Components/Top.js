f2f4d435797ffe89269f87dbdabe68dc91cd2b52
prageeth2020 <prageethpramuditha.2020@gmail.com>
2020-06-14 17:35:24 +0530

    creating <Top/>

--- src/Components/Top.js
+++ src/Components/Top.js
@@ -5,11 +5,23 @@ class Top extends Component {
     render() {
         return (
             <div className="backgroundTop">
-                <div className="container text-wrap mrgn p-2  ">
+                <div className="container p-2">
+                    <div className="row justify-content-between">
+                        <div className="col-4">
+                            <div className="form-check form-check-inline">
+                                <a href="" className=" "><i className="fab fa-facebook-f text-primary "></i></a>
+                                <a href="" className="mx-5"><i className="fas fa-envelope text-danger"></i></a>
+                            </div>
+                        </div>
+                        <div className="col-4 text-right">
+                            <div className="form-check form-check-inline ">
+                                <a href="" className=" "><i className="fas fa-phone-alt text-dark "></i></a>
+                                <label className="form-check-label " >+94 77 755 6955</label>
+                            </div>
+                        </div>
+                    </div>
 
-                    <a href="" className=" "><i className="fab fa-facebook-f text-white "></i></a>
-                    <a href="" className="mx-5"><i className="fas fa-envelope text-white"></i></a>
-                    <a href="" className=""><i className="fab fa-instagram text-white"></i></a>
+
 
 
                 </div>
