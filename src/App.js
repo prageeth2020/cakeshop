d030fa40cb1003f75351acbd7cafb98d1af713b1
ChamalHerath <chamaljanadara@gmail.com>
2020-06-15 15:08:00 +0530

    ContactUs & AboutUs Started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import './App.css';
+import './themify-icons/themify-icons.css'
 import Top from "./Components/Top";
 import NavBar from "./Components/navBar";
 import Home from "./Components/Home";
 import Slideshow from "./Components/slideshow";
+import ContactUs from "./Components/ContactUs";
+import AboutUs from "./Components/AboutUs";
 function App() {
   return (
     <div>
@@ -17,7 +20,10 @@ function App() {
             <Slideshow/>
             <Route exact path="/" component={Home} />
 
+
         </Router>
+        <ContactUs/>
+        <AboutUs/>
 
     </div>
   );
